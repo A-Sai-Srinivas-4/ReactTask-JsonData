925bdaead28ef41ba01191ba7639e4cc3375d4cd
A-Sai-Srinivas-4 <asaisrinivas4@gmail.com>
2022-10-20 10:56:07 +0530

    commited

--- src/routeComponent/ProjectDetails/index.js
+++ src/routeComponent/ProjectDetails/index.js
@@ -1,6 +1,7 @@
 //import ProjectData from "../ProjectData"
 import { useParams } from "react-router-dom"
 import Data from "../../data.json"
+import "./index.css"
 
 
 
@@ -10,7 +11,7 @@ const ProjectDetails = ({match}) => {
  
     //const projectDetailsArray = projectdetails.split(",")
 
-    console.log( projectname)
+    //console.log( projectname)
      const myArray = []
   
      const fetchData = () => {
@@ -23,18 +24,49 @@ const ProjectDetails = ({match}) => {
   
     const projDetails = fetchData() 
     //console.log(details)
-    console.log(projDetails)
+    //console.log(projDetails)
   
-   
+    const Name = []
+    const Role = []
+    const  details = {}
 
+    for (var i = 0; i < projDetails.length; i++) {
+        if(i % 2 === 0) { // index is even
+            Name.push(projDetails[i]);
+            details.Name = Name;
+        }
+        else{
+            Role.push(projDetails[i]);
+            details.Role = Role;
+        }
+    }
+    //  console.log(Name)
+    // console.log(Role)
+
+     console.log(details)
+    
+    
+    
+
+    
+
+    
 
     return(
 
         <>
-        <h1>{projectname}</h1>
-        <p> Name : {projDetails[0]}  Role :{projDetails[1]}</p>
-       
-        </>
+        <div className="project-details-card mt-5 ">
+        <center>
+        <div className="card">
+
+        <h1> Project Name : <span className="heading-text">{projectname}</span></h1> <br/>
+        <h4>Name : <span className="text">{details.Name}</span></h4>  
+        <h4>Role : <span className="text">{details.Role}</span></h4>  <br/>
+        </div>
+        </center>
+        </div>
+        
+      </>
 
     )
 }
